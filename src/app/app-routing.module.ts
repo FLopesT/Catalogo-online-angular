1f0b5e78307aa786d7caa1d172dd1163c50e9509
Felipe Lopes <89878572+FLopesT@users.noreply.github.com>
2022-03-25 22:25:36 -0300

    1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ import { CategoryComponent } from './pages/category/category.component';
 const routes: Routes = [
   { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "product/:id", component: ProductComponent },
-  { path: "category/:id", component: CategoryComponent }
+  { path: "category/:id", component: CategoryComponent },
+  { path: "**", redirectTo: "" }
 ];
 
 @NgModule({
